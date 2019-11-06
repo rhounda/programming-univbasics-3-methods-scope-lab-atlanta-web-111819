71f488ab1570b11d946bc382db3b54e5050696e2
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-06 00:31:02 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,29 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+ phrase = "it's-a me, Mario!\n"
   puts phrase
+  
 end
 
 def toadstool
-  puts status
+status = 'Thank You Mario! But Our Princess Is In Another Castle!\n'
+puts status
+
 end
+
+def link
+  say = "it's Dangerous To Go Alone! Take This.\n"
+  
+  puts say
+  
+end 
+
+def any_phrase 
+  
+  repeat = "Do A Barrel Roll!\n"
+  puts repeat
+end 
+  
+  
+  
+  
